0bc812e1fde32a6061b4b9327ac42da7dbebe980
danilaph <molotnk@yandex.ru>
2025-01-19 20:09:43 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,9 @@
+title1 = input('Введите заголовок заметки 1: ')
+title2 = input('Введите заголовок заметки 2: ')
+title3 = input('Введите заголовок заметки 3: ')
+titles = [
+    title1, title2, title3
+]
+
+print('\nВы вели следующие данные:')
+print(titles)
